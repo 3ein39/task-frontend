1cd1416b4e7ba8489a44ad4d470a711fc3f9c9e7
Hussein Hany <hussein.hany.cs@gmail.com>
2024-02-04 19:14:58 +0200

    Use LazyLoad for header & product components

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
+<script setup>
+import { defineAsyncComponent } from 'vue'
+
+const Header = defineAsyncComponent(() =>
+    import('./components/Header.vue')
+)
+
+const Product = defineAsyncComponent(() =>
+    import('./components/Product/index.vue')
+)
+</script>
+
 <template>
     <Header />
     <Product />
