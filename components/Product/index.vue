3b72dedae3a8b9e8889bc6c1ad8855bc2a8ac749
Hussein Hany <hussein.hany.cs@gmail.com>
2024-02-04 14:25:17 +0200

    feat(breadcrumbs): Create breadcrumb placeholder

---
+++ components/Product/index.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { Breadcrumb } from 'ant-design-vue'
+const routes = [
+  {
+    path: '/',
+    breadcrumbName: 'Home'
+  },
+  {
+    path : '/categories',
+    breadcrumbName: 'category 1'
+  }
+]
+</script>
+
+<template>
+
+  <Breadcrumb :routes="routes" />
+
+</template>
+
+<style scoped>
+  .ant-breadcrumb {
+    margin: 16px;
+  }
+  .ant-breadcrumb-item {
+    font-size: 20px;
+  }
+  .focus {
+    color: #1890ff;
+  }
+</style>
