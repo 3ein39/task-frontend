ea50b01d9af6f63c3347c69e734bb8bf5f04d282
Hussein Hany <hussein.hany.cs@gmail.com>
2024-02-05 19:42:37 +0200

    Remove unused imports and components

--- app.vue
+++ app.vue
@@ -1,13 +1,5 @@
 <script setup>
-import { defineAsyncComponent } from 'vue'
 
-const Header = defineAsyncComponent(() =>
-    import('./components/Header.vue')
-)
-
-const Product = defineAsyncComponent(() =>
-    import('./components/Product/index.vue')
-)
 </script>
 
 <template>
