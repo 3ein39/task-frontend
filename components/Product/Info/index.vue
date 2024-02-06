7761996612ef1bad28843107cd7ae99671f9b0ad
Hussein Hany <hussein.hany.cs@gmail.com>
2024-02-06 19:06:51 +0200

    Refactor product info component tabs

--- components/Product/Info/index.vue
+++ components/Product/Info/index.vue
@@ -1,6 +1,15 @@
 <script setup>
 import { Tabs } from 'ant-design-vue';
 import { defineProps } from 'vue';
+import { ref } from 'vue'
+
+const tabs = [
+  { title: 'معلومات', content: 'Tab 1 content' },
+  { title: 'تفاصيل', content: 'Tab 2 content' },
+  { title: 'التقييم', content: 'Tab 3 content' }
+];
+
+const activeTab = ref(0);
 
 
 const props = defineProps({
@@ -10,27 +19,35 @@ const props = defineProps({
 </script>
 
 <template>
-  <Tabs v-if="productDetails && productRatings">
-    <Tabs.TabPane key="1" tab="Info">
-      <h1>Benefits</h1>
-      <h2>{{productDetails.productGetByID.benefits}}</h2>
-      <h1>Overview</h1>
-      <h2>{{productDetails.productGetByID.overview}}</h2>
-    </Tabs.TabPane>
-    <Tabs.TabPane key="2" tab="Details">
-      <h1>Gender: {{ productDetails.productGetByID.gender }}</h1>
-      <h1>Capacity: {{ productDetails.productGetByID.capacity }}</h1>
-      <h1>Model Number: {{ productDetails.productGetByID.model_number }}</h1>
-    </Tabs.TabPane>
-    <Tabs.TabPane key="3" tab="Reviews">
-      <div v-for="rating in productRatings" :key="rating.user_id">
-        <h1>{{ rating.user.username }}: {{ rating.rating }}</h1>
-        <h2>{{ rating.comment }}</h2>
+  <div class="flex flex-col w-full px-16 py-32" dir="rtl">
+    <div class="flex mb-4">
+      <div v-for="(tab, index) in tabs" :key="index" class="cursor-pointer py-2 mx-14 px-2 tab-nav"
+        :class="{ 'tab-border in-focus': activeTab === index }" @click="activeTab = index">
+        {{ tab.title }}
       </div>
-    </Tabs.TabPane>
-  </Tabs>
+    </div>
+    <div class="tab-content">
+      <div v-for="(tab, index) in tabs" :key="index" v-show="activeTab === index">
+        {{ tab.content }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.tab-nav {
+  font-size: 32px;
+  font-weight: 700;
+  color: #939393;
+  font-family: 'Montserrat-Arabic';
+}
+
+.tab-border {
+  border-bottom: 6px solid #48B1A6;
+}
+
+.in-focus {
+  color : black;
+}
 
 </style>
