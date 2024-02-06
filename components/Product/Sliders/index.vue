5902782b09aabc0e73f8453416021469654b26ea
Hussein Hany <hussein.hany.cs@gmail.com>
2024-02-06 13:32:44 +0200

    Add GraphQL queries to load products and average ratings

--- components/Product/Sliders/index.vue
+++ components/Product/Sliders/index.vue
@@ -1,10 +1,50 @@
 <script setup>
+import { gql, request } from 'graphql-request'
+import { ref } from "vue";
+
+const endpoint = 'http://localhost:4000/graphql'
+let products = ref(null);
+
+
+const loadProducts = async () => {
+  const query = gql`
+    query {
+      productGetAllWithImages {
+        product_id
+        title
+        description
+        price
+        discounted_price
+        images {
+          url
+        }
+      } 
+    }
+  `
+  products.value = await request(endpoint, query)
+  // console.log(products.value)
+}
+loadProducts()
+
+const loadAvgRating = async (product_id) => {
+  const query = gql`
+    query {
+      productGetAvgRatingByID(product_id: ${product_id}) {
+        avg_rating
+      }
+    }
+  `
+  const data = await request(endpoint, query)
+  return data.productGetAvgRatingByID.avg_rating
+}
+
+products.value?.productGetAllWithImages.forEach(async (product) => {
+  product.avg_rating = await loadAvgRating(product.product_id)
+})
 
 </script>
 
 
 <template>
-  <h1> this is sliders </h1>
-  <LazyProductSlidersCard />
-
+  <LazyProductSlidersCard :products="products.productGetAllWithImages"/>
 </template>
