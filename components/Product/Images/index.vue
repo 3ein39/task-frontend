689216a6c62fe86805daf84043d2d3bf7d410589
Hussein Hany <hussein.hany.cs@gmail.com>
2024-02-09 22:58:22 +0200

    Refactor image zoom functionality

--- components/Product/Images/index.vue
+++ components/Product/Images/index.vue
@@ -1,17 +1,26 @@
 <template>
   <div v-if="productImages" class="image-carousel">
     <div class="thumbnail-images">
-      <div v-for="(image, index) in productImages.slice(1)" :key="index" class="thumbnail-image" @click="swapImage(image)">
+      <div v-for="(image, index) in productImages.slice(1)" :key="index" class="thumbnail-image"
+        @click="swapImage(image)">
         <img :src="image.url" alt="Thumbnail Image" width="50" height="50">
       </div>
     </div>
     <div class="main-image relative">
-      <img :src="mainImage.url" alt="Main Image" width="373" height="520">
-      <svg @click="toggleFav" xmlns="http://www.w3.org/2000/svg" :fill="svgColor" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#939393"
-        :class="locale === 'en' ? 'w-12 h-12 absolute top-0 left-10' : 'w-12 h-12 absolute top-0 right-10'">
+      <img :class="{'zoom': zoom}" :src="mainImage.url" alt="Main Image" width="373" height="520">
+      <svg @click="toggleFav" xmlns="http://www.w3.org/2000/svg" :fill="svgColor" viewBox="0 0 24 24" strokeWidth={1.5}
+        stroke="#939393"
+        :class="locale === 'en' ? 'w-12 h-12 absolute top-0 left-10 cursor-pointer' : 'w-12 h-12 absolute top-0 right-10 cursor-pointer'">
         <path strokeLinecap="round" strokeLinejoin="round"
           d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
       </svg>
+        <svg @click="zoomIn" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#939393"
+          :class="locale === 'en' ? 'w-12 h-12 absolute bottom-0 right-10 cursor-pointer' : 'w-12 h-12 absolute bottom-0 left-10 cursor-pointer'">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607ZM10.5 7.5v6m3-3h-6" />
+        </svg>
+
+
     </div>
   </div>
 </template>
@@ -33,6 +42,8 @@ let svgColor = ref('white');
 
 
 
+
+
 const swapImage = (image) => {
   let temp = mainImage.value;
   mainImage.value = image;
@@ -47,14 +58,24 @@ const toggleFav = () => {
   }
 }
 
-const zoomIn = () => {
+const zoomIn = (event) => {
   const mainImage = document.querySelector('.main-image');
-  mainImage.style.transform = 'scale(1.3)';
-}
+  mainImage.classList.add('zoomed');
+
+  mainImage.addEventListener('click', (event) => {
+    if (event.target === mainImage) {
+      zoomOut();
+    }
+  });
+  document.addEventListener('keydown', (event) => {
+      zoomOut();
+  });
+};
+
 
 const zoomOut = () => {
   const mainImage = document.querySelector('.main-image');
-  mainImage.style.transform = 'scale(1)';
+  mainImage.classList.remove('zoomed');
 }
 
 </script>
@@ -90,6 +111,24 @@ const zoomOut = () => {
   cursor: pointer;
 }
 
+.zoomed {
+  transform: scale(1.5);
+  transition: transform 0.25s ease;
+  z-index: 1000;
+  position: fixed;
+  top: 50%;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  overflow: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: 'zoom-out';
+  z-index: 1000;
+  top: 0;
+}
+
 
 @media (max-width: 1193px) {
   .image-carousel {
@@ -100,5 +139,4 @@ const zoomOut = () => {
     display: flex;
     flex-direction: row;
   }
-}
-</style>
+}</style>
