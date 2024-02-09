b80d1635156c837ccf71e7708d837cedcfa5ecb7
Hussein Hany <hussein.hany.cs@gmail.com>
2024-02-09 22:20:37 +0200

    Add favorite toggle functionality to main image

--- components/Product/Images/index.vue
+++ components/Product/Images/index.vue
@@ -5,14 +5,22 @@
         <img :src="image.url" alt="Thumbnail Image" width="50" height="50">
       </div>
     </div>
-    <div class="main-image">
-      <img :src="productImages[0].url" alt="Main Image" width="373" height="520" @mouseover="zoomIn" @mouseout="zoomOut">
+    <div class="main-image relative">
+      <img :src="productImages[0].url" alt="Main Image" width="373" height="520">
+      <svg @click="toggleFav" xmlns="http://www.w3.org/2000/svg" :fill="svgColor" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#939393"
+        :class="locale === 'en' ? 'w-12 h-12 absolute top-0 left-10' : 'w-12 h-12 absolute top-0 right-10'">
+        <path strokeLinecap="round" strokeLinejoin="round"
+          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+      </svg>
     </div>
   </div>
 </template>
 
 <script setup>
 import { defineProps } from 'vue';
+let locale = inject('locale');
+
+let svgColor = ref('white');
 
 const props = defineProps({
   productImages: {
@@ -20,6 +28,14 @@ const props = defineProps({
     required: true,
   },
 });
+
+const toggleFav = () => {
+  if (svgColor.value === 'white') {
+    svgColor.value = '#FF0000';
+  } else {
+    svgColor.value = 'white';
+  }
+}
 
 const zoomIn = () => {
   const mainImage = document.querySelector('.main-image');
