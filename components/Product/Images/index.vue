2a148dc4e9016496c2f9200639e98121dc7d4810
Hussein Hany <hussein.hany.cs@gmail.com>
2024-02-13 13:54:02 +0200

    Implement share button functionality

--- components/Product/Images/index.vue
+++ components/Product/Images/index.vue
@@ -15,9 +15,9 @@
           d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
       </svg>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#939393"
-          :class="locale === 'en' ? 'w-12 h-12 absolute top-12 left-10 cursor-pointer' : 'w-12 h-12 absolute top-12 right-10 cursor-pointer'">
+        :class="locale === 'en' ? 'w-12 h-12 absolute top-12 left-10 cursor-pointer' : 'w-12 h-12 absolute top-12 right-10 cursor-pointer'"
+        @click="share">
         <path stroke-linecap="round" stroke-linejoin="round"
-        
           d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
       </svg>
 
@@ -30,6 +30,22 @@
       </svg>
 
 
+    </div>
+  </div>
+
+  <div v-if="showShareModal" class="fixed inset-0 flex items-center justify-center z-50">
+    <div class="absolute inset-0 bg-black opacity-50"></div>
+    <div class="bg-white p-6 rounded shadow-lg z-10" style="width: 50%;">
+      <p class="mb-4 text-lg font-semibold">Copy and paste this link to share:</p>
+      <div class="mb-4 relative">
+        <input type="text" v-model="shareableLink" readonly class="w-full px-4 py-2 border border-gray-300 rounded">
+        <button class="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500" @click="copyToClipboard">
+          Copy
+        </button>
+      </div>
+      <button @click="showShareModal = false" class="w-full py-2 text-white bg-blue-500 rounded hover:bg-blue-600">
+        Close
+      </button>
     </div>
   </div>
 </template>
@@ -50,8 +66,34 @@ let mainImage = ref(props.productImages[0]);
 let svgColor = ref('white');
 
 
+// not supported by many browsers
+// fix: Modal fallback
+let shareableLink = ref(window.location.href);
+let showShareModal = ref(false);
+const share = () => {
+  if (navigator.share) {
+    navigator.share({
+      title: 'WebShare API Demo',
+      url: shareableLink.value,
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch(console.error);
+  } else {
+    console.log('Web Share API not supported');
+    showShareModal.value = true;
+  }
+};
 
-
+// the copy to clipboard function
+const copyToClipboard = () => {
+  const el = document.createElement('textarea');
+  el.value = shareableLink.value;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
 
 const swapImage = (image) => {
   let temp = mainImage.value;
@@ -148,4 +190,5 @@ const zoomOut = () => {
     display: flex;
     flex-direction: row;
   }
-}</style>
+}
+</style>
