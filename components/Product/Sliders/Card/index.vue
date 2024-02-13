93427c71d0c92a55226a8106e21898f584e0d129
Hussein Hany <hussein.hany.cs@gmail.com>
2024-02-13 20:06:31 +0200

    Fix star rating display in product card

--- components/Product/Sliders/Card/index.vue
+++ components/Product/Sliders/Card/index.vue
@@ -104,13 +104,13 @@ const toggleFav = (id) => {
               <h4 class="mt-2 font-semibold text-m leading-tight">{{ product.description }} </h4>
 
               <div class="flex">
-                <svg v-for="n in Math.floor(product.avg_rating)" :key="n" xmlns="http://www.w3.org/2000/svg" fill="yellow"
+                <svg v-for="n in (Math.floor(product.avg_rating) ? Math.floor(product.avg_rating) : 0)" :key="n" xmlns="http://www.w3.org/2000/svg" fill="yellow"
                   viewBox="0 0 24 24" stroke-width="1.5" stroke="yellow" class="w-6 h-6">
                   <path stroke-linecap="round" stroke-linejoin="round"
                     d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
                 </svg>
                 <!-- remaining stars not filled -->
-                <svg v-for="n in 5 - Math.floor(product.avg_rating)" :key="n" xmlns="http://www.w3.org/2000/svg"
+                <svg v-for="n in 5 - (Math.floor(product.avg_rating) ? Math.floor(product.avg_rating) : 0)" :key="n" xmlns="http://www.w3.org/2000/svg"
                   fill="#F5BD3661" viewBox="0 0 24 24" stroke-width="1.5" stroke="yellow" class="w-6 h-6">
                   <path stroke-linecap="round" stroke-linejoin="round"
                     d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
