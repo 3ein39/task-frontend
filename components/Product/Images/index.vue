ceb948c4e55dacc6230f1f088a319ba471bee804
Hussein Hany <hussein.hany.cs@gmail.com>
2024-02-13 13:42:44 +0200

    Style svg share icon

--- components/Product/Images/index.vue
+++ components/Product/Images/index.vue
@@ -7,18 +7,27 @@
       </div>
     </div>
     <div class="main-image relative">
-      <img :class="{'zoom': zoom}" :src="mainImage.url" alt="Main Image" width="373" height="520">
+      <img :class="{ 'zoom': zoom }" :src="mainImage.url" alt="Main Image" width="373" height="520">
       <svg @click="toggleFav" xmlns="http://www.w3.org/2000/svg" :fill="svgColor" viewBox="0 0 24 24" strokeWidth={1.5}
         stroke="#939393"
         :class="locale === 'en' ? 'w-12 h-12 absolute top-0 left-10 cursor-pointer' : 'w-12 h-12 absolute top-0 right-10 cursor-pointer'">
         <path strokeLinecap="round" strokeLinejoin="round"
           d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
       </svg>
-        <svg @click="zoomIn" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#939393"
-          :class="locale === 'en' ? 'w-12 h-12 absolute bottom-0 right-10 cursor-pointer' : 'w-12 h-12 absolute bottom-0 left-10 cursor-pointer'">
-          <path stroke-linecap="round" stroke-linejoin="round"
-            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607ZM10.5 7.5v6m3-3h-6" />
-        </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#939393"
+          :class="locale === 'en' ? 'w-12 h-12 absolute top-12 left-10 cursor-pointer' : 'w-12 h-12 absolute top-12 right-10 cursor-pointer'">
+        <path stroke-linecap="round" stroke-linejoin="round"
+        
+          d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
+      </svg>
+
+
+      <svg @click="zoomIn" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+        stroke="#939393"
+        :class="locale === 'en' ? 'w-12 h-12 absolute bottom-0 right-10 cursor-pointer' : 'w-12 h-12 absolute bottom-0 left-10 cursor-pointer'">
+        <path stroke-linecap="round" stroke-linejoin="round"
+          d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607ZM10.5 7.5v6m3-3h-6" />
+      </svg>
 
 
     </div>
@@ -68,7 +77,7 @@ const zoomIn = (event) => {
     }
   });
   document.addEventListener('keydown', (event) => {
-      zoomOut();
+    zoomOut();
   });
 };
 
